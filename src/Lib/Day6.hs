8eb4a3ac231125999ccbf6dd1904e2e97aaa74cb
Nik Klassen <nikklassen@users.noreply.github.com>
2018-12-10 23:52:41 -0500

    Day 6

--- src/Lib/Day6.hs
+++ src/Lib/Day6.hs
@@ -1,90 +1,68 @@
-{-# LANGUAGE NamedFieldPuns, LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module Lib.Day6 (
     run
 ) where
 
-import Prelude hiding (lookup)
-import Data.Ord (comparing)
-import Lib.Matrix (Matrix, mapMatrixWithKey, (!), fromList, setElem, elems, foldMatrix, lookup)
-import Data.List (minimumBy, maximumBy, findIndex)
+import Lib.Matrix (Matrix, (!), fromList, setElem, foldMatrix)
+import Data.List (findIndex)
 import Data.Maybe (mapMaybe)
-import Lib.Util (trace1, mapSnd)
+import Lib.Util (mapSnd)
 import qualified Data.IntMap.Lazy as M
 
-data FillState = Empty | Center Int | DMZ | Filled Int
-    deriving (Show)
+data FillState = Empty | DMZ | Filled Int deriving (Show, Eq)
 
 type Fringe = [((Int, Int), FillState)]
 
 getBounds :: [(Int, Int)] -> ((Int, Int), (Int, Int))
-getBounds ps =
-    let xs = map fst ps
-        ys = map snd ps
-        minX = minimum xs
-        maxX = maximum xs
-        minY = minimum ys
-        maxY = maximum ys
-    in ((minX, maxX), (minY, maxY))
+getBounds = getBounds' ((maxBound, minBound), (maxBound, minBound))
+    where getBounds' ((xmin, xmax), (ymin, ymax)) ((x, y):ps) = getBounds' ((min x xmin, max x xmax), (min y ymin, max y ymax)) ps
+          getBounds' bs [] = bs
 
-filterInf ((minX, maxX), (minY, maxY)) =
-    filter (\(x, y) -> x > minX && x < maxX && y > minY && y < maxY)
-
-adjacent (x, y) (xbs, ybs) = [
-    (x + dx, y + dy) | dx <- [-1..1],
-                       dy <- [-1..1],
-                       not (dx == dy && dx == 0) && inRange (x + dx) xbs && inRange (y + dy) ybs]
+adjacent (x, y) (xbs, ybs) = filter (\(x, y) -> inRange x xbs && inRange y ybs) $ map (\(dx, dy) -> (x + dx, y + dy)) [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
 inRange x (xMin, xMax) = x >= xMin && x <= xMax
 
 addToFringe :: Fringe -> ((Int, Int), FillState) -> Fringe
 addToFringe fringe (x, f) =
-    case findIndex ((==) x . fst) fringe of
-        Just i -> let (ys, _:zs) = splitAt i fringe in ys ++ (x, DMZ):zs
+    case findIndex ((== x) . fst) fringe of
         Nothing -> (x, f):fringe
-
-fill (Center c) = Just $ Filled c
-fill (Filled c) = Just $ Filled c
-fill DMZ = Nothing
-fill Empty = Nothing
+        Just i ->
+            if snd (fringe !! i) == f then
+                fringe
+            else
+               let (ys, _:zs) = splitAt i fringe in ys ++ (x, DMZ):zs
 
 proliferate' :: ((Int, Int), (Int, Int)) -> (Fringe, Matrix FillState) -> (Fringe, Matrix FillState)
 proliferate' bs (fringe, m) =
-    let unfilled p = filter (isEmpty . (!) m) $ adjacent p bs
-        fringe' = foldl addToFringe [] $ concatMap (\(p, f) -> mapMaybe (\p -> (,) p <$> fill f) (unfilled p)) fringe
+    let unfilled p = filter ((== Empty) . (!) m) $ adjacent p bs
+        fringe' = foldl addToFringe [] $ concatMap (\(p, f) -> map (, f) $ unfilled p) $ filter (\(_, f) -> isFilled f) fringe
         m' = foldl (\m (p, f) -> setElem p f m) m fringe'
     in (filter (isFilled . snd) fringe', m')
 
-isFilled (Center c) = True
-isFilled (Filled c) = True
+isFilled (Filled _) = True
 isFilled _ = False
-
-isEmpty Empty = True
-isEmpty _ = False
-
-getKey (Center c) = c
-getKey (Filled c) = c
-getKey _ = 0
 
 findBiggest :: Matrix FillState -> Int
 findBiggest = maximum . M.elems . foldMatrix go M.empty
-    where go acc s =
-            let k = getKey s
-            in if k == 0 then
-                acc
-            else
-                M.insert k (M.findWithDefault 0 k acc) acc
+    where go acc (Filled i) = M.insert i (1 + M.findWithDefault 0 i acc) acc
+          go acc _ = acc
 
-printMat m =
-    unlines $ map (\x -> unwords $ map (\y -> go (x, y)) [1..9]) [1..9]
-    where go idx =
-            let k = maybe 0 getKey $ lookup idx m
-            in if k == 0 then "." else show k
+distanceToCenter (x, y) (cx, cy) = abs (cx - x) + abs (cy - y)
 
-run = do
-    f <- readFile "input\\day06_test"
+getGrid = do
+    f <- readFile "input\\day06"
     let points = map (\p -> read $ "(" ++ p ++ ")") $ lines f :: [(Int, Int)]
-    let bs@((minX, maxX), (minY, maxY)) = getBounds points
+    return (getBounds points, points)
+
+part1 = do
+    (bs@((minX, maxX), (minY, maxY)), points) <- getGrid
     let mInit = fromList [((x, y), Empty) | x <- [minX..maxX], y <- [minY..maxY]]
-    let m = foldl (\acc (i, idx) -> setElem idx (Center i) acc) mInit $ zip [1..] points
-    -- print $ until (all notEmpty . map snd . elems) (proliferate bs) m
-    putStrLn $ printMat $ snd $ until (null . fst) (proliferate' bs) (map (mapSnd Center) (zip points [1..]), m)
+    let centers = zip points $ map Filled [1..]
+    let m = foldl (\acc (idx, c) -> setElem idx c acc) mInit centers
+    print $ findBiggest $ snd $ until (null . fst) (proliferate' bs) (centers, m)
+
+part2 = do
+    (((minX, maxX), (minY, maxY)), points) <- getGrid
+    print $ length $ filter (\p -> sum (map (distanceToCenter p) points) < 10000) [(x, y) | x <- [minX..maxX], y <- [minY..maxY]]
+
+run = part1
